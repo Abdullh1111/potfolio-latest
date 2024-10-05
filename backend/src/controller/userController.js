23c495f7ce4221cdc8e8e7fe92616fd833a6cd12
Abdullh1111 <abdullah4474032@gmail.com>
2024-10-05 22:51:48 +0600

    update dashboard

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -194,7 +194,7 @@ export const updatePassword = catchAsyncErrors(async (req, res, next) => {
 });
 
 export const getUserForPortfolio = catchAsyncErrors(async (req, res, next) => {
-  const id = "663296a896e553748ab5b0be";
+  const id = "67016cb55cb06fd9e0efcd48";
   const user = await User.findById(id);
   res.status(200).json({
     success: true,
